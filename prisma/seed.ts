d527bdfdfc19f08b66d91d9bbc0f9532d85451c9
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2025-03-16 19:43:10 -0300

    Set creatAt and updatedAt on tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { hash, hashSync } from "bcrypt-ts";
+import { hashSync } from "bcrypt-ts";
 
 const prisma = new PrismaClient();
 
 async function main() {
   const root = await prisma.user.upsert({
-    where: { email: "rodrigoantunestutz@gmail.com" },
+    where: { email: process.env.ROOT_DATABASE_EMAIL },
     update: {},
     create: {
       email: process.env.ROOT_DATABASE_EMAIL ?? "root@mail.com",
       name: process.env.ROOT_DATABASE_NAME ?? "Administrador",
       password: hashSync(process.env.ROOT_DATABASE_PASSWORD ?? "123456789"),
       confirmed: true,
-      confirmToken: null
+      confirmToken: null,
     },
   });
 
