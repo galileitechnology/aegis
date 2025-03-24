b31d9f0b5b8f6cc15b815dce50147a75b534f720
Rodrigo Tutz <rodrigoantunestutz@gmail.com>
2025-03-23 23:22:38 -0300

    fix sidebar mobile view

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,14 +6,14 @@ export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <SidebarProvider defaultOpen={true} className="bg-neutral-900">
-      <SidebarTrigger className="absolute top-0 left-0 z-50 text-white">
+    <SidebarProvider defaultOpen={true} className="bg-neutral-900 relative">
+      <SidebarTrigger className="absolute md:hidden top-0 left-0 z-50 text-white">
         <Menu size={24} />
       </SidebarTrigger>
       <aside>
         <AppSidebar />
       </aside>
-      <main className="w-full rounded-lg m-3 text-white bg-neutral-950">
+      <main className="w-full rounded-lg md:m-3 pt-3 text-white bg-neutral-950">
         {children}
       </main>
     </SidebarProvider>
