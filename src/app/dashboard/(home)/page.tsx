39835a6b2863ae007638a866684c7960c0de0f01
Lucas Borges <galileitechnology@gmail.com>
2025-08-05 11:44:01 -0300

    AEGIS Reballing after pc died

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -1,61 +1,24 @@
-"use client"
-
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle, } from '@/components/ui/card'
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent, } from '@/components/ui/chart'
-
-const chartData = [
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-  { up: 1 },
-]
-
-const chartConfig = {
-  up: {
-    label: "Desktop",
-    color: "#46C001",
-  },
-  down: {
-    label: "Desktop",
-    color: "#ff0000",
-  },
-} satisfies ChartConfig
-
 export default function Page() {
   return (
-    <ChartContainer config={chartConfig} className=" relative h-[200px] w-[800px] bg-[#5200ff] p-5">
-      <BarChart accessibilityLayer data={chartData}>
-        <XAxis
-          dataKey="time"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
-        />
-        <Bar dataKey="up" fill="var(--color-up)" />
-        <Bar dataKey="down" fill="var(--color-down)" />
-      </BarChart>
-    </ChartContainer>
+    <div className="inline-grid grid-cols-3 gap-0">
+      <div className="w-[300px] h-[150px] p-2">
+        <div className="w-[100%] h-[100%] bg-[#111111] border-1">
+          <p className="pl-2 text-[#909090] text-[15px]">Requests Lifeline</p>
+          <p className="pl-2 text-[#2bff00] text-[50px] pt-2 ">No data</p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[150px] p-2">
+        <div className="w-[100%] h-[100%] bg-[#111111] border-1">
+          <p className="pl-2 text-[#909090] text-[15px]">Databases Lifeline</p>
+          <p className="pl-2 text-[#2bff00] text-[50px] pt-2 ">No data</p>
+        </div>
+      </div>
+      <div className="w-[300px] h-[150px] p-2">
+        <div className="w-[100%] h-[100%] bg-[#111111] border-1">
+          <p className="pl-2 text-[#909090] text-[15px]">Offline Applications</p>
+          <p className="pl-2 text-[#2bff00] text-[50px] pt-2 ">No data</p>
+        </div>
+      </div>
+    </div>
   )
 }
